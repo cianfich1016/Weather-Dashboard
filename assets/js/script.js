c528283e47f4fac80bce4441929d4630b7e3db76
Rachel Cianfichi <cianfich@gmail.com>
2021-09-26 21:56:07 -0400

    Edited script.js file with API key and URL requested to make first fetch request once city is entered and button is clicked.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var APIKey = "04c801176447e332047f2f7bc4868de7";
+
+
+var searchButton = document.getElementById("searchBtn")
+
+function getWeather(event){
+    event.preventDefault()
+    var city = document.getElementById("inputCity").value;
+    
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=city&appid=04c801176447e332047f2f7bc4868de7";
+    
+    fetch(queryURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        });
+    };
+
+searchButton.addEventListener("click", getWeather);
