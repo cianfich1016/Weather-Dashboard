37422bae87f4de36378ae920ac737f57da9c125b
Rachel Cianfichi <cianfich@gmail.com>
2021-09-27 10:10:08 -0400

    Added second fetch request with actual lat and lon used to return information about specific city.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
 var APIKey = "04c801176447e332047f2f7bc4868de7";
+var searchButton = document.getElementById("searchBtn");
 
-
-var searchButton = document.getElementById("searchBtn")
 
 function getWeather(event){
     event.preventDefault();
     var city = document.getElementById("inputCity").value;
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=04c801176447e332047f2f7bc4868de7";
+
+    var currentHeader = document.querySelector(".currentHeader");
+    var chosenCity = document.querySelector(".display-4");  
+    chosenCity.textContent = city;
+    
     
     fetch(queryURL)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
+        //console.log(data);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        //console.log(lat);
+        //console.log(lon);
+        
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=04c801176447e332047f2f7bc4868de7"
+
+        fetch(queryURL2)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
             console.log(data);
+            
+            })
+        
         });
+        
     };
 
 searchButton.addEventListener("click", getWeather);
+
