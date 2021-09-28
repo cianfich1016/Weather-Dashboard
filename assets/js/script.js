68a03242add2655142f0a3af2eaa1b129a97b557
Rachel Cianfichi <cianfich@gmail.com>
2021-09-28 10:16:49 -0400

    Added color badges to UV index.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var APIKey = "04c801176447e332047f2f7bc4868de7";
 var searchButton = document.getElementById("searchBtn");
 var jumbotron = document.querySelector(".jumbotron");
-var fiveDay = document.querySelector("h3")
+var fiveDay = document.querySelector("h3");
+var uvInfo = document.querySelector("span");
 
 function getCity(event){
     event.preventDefault();
 
     jumbotron.style.display = "block";
+    
 
     var city = document.getElementById("inputCity").value;
         var chosenCity = document.querySelector(".display-4"); 
@@ -52,14 +54,22 @@ function getWeather(queryURL2){
             var humidity = document.querySelector(".lead3");
             humidity.textContent = "Humidity: " + data.current.humidity + "%";
 
-            //var uv = document.querySelector(".lead4");
-            //var uvIndex = document.querySelector("span");
+            var uv = document.querySelector(".lead4");
+            var uvIndex = document.createElement("span")
+            uvIndex = data.current.uvi;
+            uv.textContent = uvIndex;
 
-            //uvIndex.textContent = data.current.uvi;
-            
-            //uv.textContent = "UV Index: " + uvIndex.textContent;
+            if (uvIndex >= 3 && uvIndex <= 7.99){
+                uv.setAttribute("class", " badge badge-warning")
+            } else if(uvIndex >= 8 && uvIndex <= 10.99){
+                uv.setAttribute("class", "badge badge-danger")
+            } else if(uvIndex >= 11){
+                uv.setAttribute("class", "badge badge-dark");
+            };
+        
             
             fiveDay.style.display = "block";
+
             
             for (var i=0; i<5; i++){
                 var forecastEL = document.querySelector("#futureWeather");
@@ -79,11 +89,12 @@ function getWeather(queryURL2){
 
                 futureDate.setAttribute("style", "font-size: 20px; font-weight: bold");
 
-                var linkImage = data.daily[i].weather[0].icon;
-                console.log(linkImage)
+                futureImage.setAttribute("src", "http://openweathermap.org/img/wn/10d@2x.png")
+
+                //var linkImage = data.daily[i].weather[0].icon;
 
                 futureDate.textContent = moment().add(1, "days").format("l");
-                futureImage.src = "http://openweathermap.org/img/wn/" + linkImage + "@2x.png";
+            
                 futureTemp.textContent = "Temp: " + data.daily[i].temp.day + "\xB0F";
                 futureWind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
                 futureHumidity.textContent = "Humidity: " + data.daily[i].humidity + "%";
