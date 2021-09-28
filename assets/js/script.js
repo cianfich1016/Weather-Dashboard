84d0ae2f8aba3259a4836dd8fb6ba540bf3252df
Rachel Cianfichi <cianfich@gmail.com>
2021-09-28 15:40:47 -0400

    Added comments to all files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
+//Generated API key from Open Weather Map
 var APIKey = "04c801176447e332047f2f7bc4868de7";
 var searchButton = document.getElementById("searchBtn");
 var jumbotron = document.querySelector(".jumbotron");
 var fiveDay = document.querySelector("h3");
 var uvInfo = document.querySelector("span");
 var mainWeather = document.querySelector(".mainWeather");
+//Created array to push names of previous cities searched"
 var cities = JSON.parse(localStorage.getItem("cities")) || [];
-console.log(cities)
+
+//On the click of the previous cities searched, the same weather information will display by calling the main getCity(city) function.
 document.querySelector(".previousSearch").addEventListener("click", function(e){
     e.preventDefault();
     var city = e.target.textContent;
-    console.log(city)
     getCity(city);
 })
-
+//Created buttons for each previous city searched.
 if(cities.length){
     for(var i=0; i < cities.length; i++){
         var cityEl = document.createElement("button");
@@ -20,10 +22,11 @@ if(cities.length){
         document.querySelector(".previousSearch").appendChild(cityEl);
     }
 }
-
+//Function to get city name from user input.
 function getCity(city){
     jumbotron.style.display = "block";
 
+    //Add city to array and container of previously chosen cities.
     cities.push(city)
     localStorage.setItem("cities", JSON.stringify(cities));
 
@@ -31,11 +34,13 @@ function getCity(city){
         cityEl.textContent = city;
         document.querySelector(".previousSearch").appendChild(cityEl);
 
+    //User typed city.    
     var chosenCity = document.querySelector(".display-4"); 
+    
+    //Get current date.
     var date = moment().format("l"); 
     
-       
-    
+    //First API URL using the city name to get latitude and longitude coordinates.
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=04c801176447e332047f2f7bc4868de7";
   
     fetch(queryURL)
@@ -47,7 +52,7 @@ function getCity(city){
         var lat = data.coord.lat;
         var lon = data.coord.lon;
         
-        
+        //Used latitude and longitude data from first call to create a second URL and actually locate city to get weather.
         var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=04c801176447e332047f2f7bc4868de7"
 
         chosenCity.textContent = city + " (" + date + ")"
@@ -58,13 +63,14 @@ function getCity(city){
 };
 
 function getWeather(queryURL2){
+        //Second API call
         fetch(queryURL2)
             .then(function(response){
                 return response.json();
             })
             .then(function(data){
-            console.log(data);
-
+            
+            //Create main display of current weather using returned data from API.
             mainWeather.style.display = "block";
 
             var temp = document.querySelector(".lead1");
@@ -75,7 +81,8 @@ function getWeather(queryURL2){
 
             var humidity = document.querySelector(".lead3");
             humidity.textContent = "Humidity: " + data.current.humidity + "%";
-
+            
+            //Created uv index adding a span tag to allow for different color badges to show based on uv index number.
             var uv = document.querySelector(".lead4");
             var uvIndex = document.createElement("span")
             uvIndex = data.current.uvi;
@@ -92,7 +99,7 @@ function getWeather(queryURL2){
             
             fiveDay.style.display = "block";
 
-            
+            //Loop through daily data to retrieve information for future forecast.
             for (var i=0; i<5; i++){
                 var forecastEL = document.querySelector("#futureWeather");
                 var futureForecast = document.createElement("ul");
@@ -114,6 +121,7 @@ function getWeather(queryURL2){
 
                 futureDate.textContent = moment().add(i+1, "days").format("l");
 
+                //Linked image for icons from API website and daily data.
                 var linkImage = data.daily[i].weather[0].icon;
                 futureImage.setAttribute("src", "https://openweathermap.org/img/wn/" + linkImage + "@2x.png")
             
@@ -126,6 +134,7 @@ function getWeather(queryURL2){
         });
 }; 
 
+//Click of button event listener to start function once city is typed by user.
 searchButton.addEventListener("click", function(e){
     e.preventDefault();
     var city = document.getElementById("inputCity").value;
